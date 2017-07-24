191fd65d329ad5dcb610572f40bbc4d1e78b423d
Johannes <Johannes@Jojo-Desktop>
2017-07-24 14:52:43 +0200

    Init

---
+++ Maze/src/generator/HtmlBase.cpp
@@ -0,0 +1,36 @@
+/*
+ * HtmlBase.cpp
+ *
+ *  Created on: 24.07.2017
+ *      Author: Johannes
+ */
+
+#include "HtmlBase.h"
+#include <iostream>
+
+namespace Maze
+{
+
+namespace generator
+{
+
+HtmlBase::HtmlBase()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+HtmlBase::~HtmlBase()
+{
+	// TODO Auto-generated destructor stub
+}
+
+}/* namespace generator */
+
+} /* namespace Maze */
+
+int main()
+{
+	std::cout << "hello world";
+	return 0;
+}
---
+++ Maze/src/generator/HtmlBase.h
@@ -0,0 +1,25 @@
+/*
+ * HtmlBase.h
+ *
+ *  Created on: 24.07.2017
+ *      Author: Johannes
+ */
+
+#ifndef SRC_GENERATOR_HTMLBASE_H_
+#define SRC_GENERATOR_HTMLBASE_H_
+
+namespace Maze {
+
+namespace generator {
+class HtmlBase
+{
+public:
+	HtmlBase();
+	virtual ~HtmlBase();
+};
+
+}/* namespace generator */
+
+} /* namespace Maze */
+
+#endif /* SRC_GENERATOR_HTMLBASE_H_ */
